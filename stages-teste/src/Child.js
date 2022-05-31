5309635a39ad437620b60e32c15ed637aaf0ad41
Hermes Luciano Monteiro Junior <hermeslmj@gmail.com>
2022-05-31 14:00:11 -0300

    Add files via upload

---
+++ stages-teste/src/Child.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Box, Loader, getTheme } from "styled-minimal";
+
+const Child = ({ color = "yellow" }) => {
+  const { colors } = getTheme();
+
+  return (
+    <Box
+      alignItems="center"
+      bg="transparent"
+      border={`2px solid ${colors[color]}`}
+      borderRadius="8px"
+      display="flex"
+      height={128}
+      justifyContent="center"
+    >
+      <Loader color={colors[color]} size={48} />
+    </Box>
+  );
+};
+
+export default Child;
---
+++ stages-teste/src/Parent.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container, Heading } from "styled-minimal";
+import "./styles.css";
+
+import Child from "./Child";
+
+export default function Parent() {
+  return (
+    <Container>
+      <Heading mb={4} textAlign="center">
+        Stages
+      </Heading>
+      <Child />
+    </Container>
+  );
+}
