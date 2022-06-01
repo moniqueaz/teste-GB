037a9acad08b1e1b47b0e9ddc22cf92269107810
Monique Azevedo <monique.azevedo@b2wdigital.com>
2022-06-01 16:40:01 -0300

    teste: monique azevedo

---
+++ stages/src/Child.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect} from "react";
+import { Box, Loader, getTheme } from "styled-minimal";
+
+const Child = ({ color = "yellow", duration = '1000', start, callback }) => {
+  const [loading, setLoading] = useState(start)
+  const { colors } = getTheme();
+
+  useEffect(() => {
+    setTimeout(() => {
+        setLoading(false)
+        callback()
+      }, duration);
+  }, []);
+
+  if(loading){
+    return (<Box
+    alignItems="center"
+    bg="transparent"
+    border={`2px solid ${colors[color]}`}
+    display="flex"
+    height={128}
+    justifyContent="center"
+  >
+    <Loader color={colors[color]} size={48} />
+  </Box>)
+  }
+
+  return (<Box
+        alignItems="center"
+        bg={`${colors[color]}`}
+        border={`2px solid ${colors[color]}`}
+        display="flex"
+        height={128}
+        justifyContent="center"
+      >
+  </Box>)
+};
+
+export default Child;
